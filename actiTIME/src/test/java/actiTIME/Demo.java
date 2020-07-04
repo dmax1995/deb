c6c6126ee2fa7f393507e30507966f588c1335df
Deb_pc <Deb_pc@192.168.1.6>
2020-07-04 15:30:47 +0530

    demo commit

---
+++ actiTIME/src/test/java/actiTIME/Demo.java
@@ -0,0 +1,12 @@
+package actiTIME;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void run() {
+		Reporter.log("my test run", true);
+	}
+
+}
