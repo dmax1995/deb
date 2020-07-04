9c40e49dbaeea79b3c1e0d153fef2261eb280097
Deb_pc <Deb_pc@192.168.1.6>
2020-07-04 15:37:37 +0530

    remo commit

---
+++ actiTIME/src/test/java/actiTIME/Remo.java
@@ -0,0 +1,12 @@
+package actiTIME;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Remo {
+	@Test
+	public void run() {
+		Reporter.log("my test run", true);
+	}
+
+}
